14dd406370677d3afd8c808b5e4b08e5f27bf7dd
Muhammad Hussein <muhammadhussein2312@gmail.com>
2025-03-06 09:33:46 +0200

    feat(eureka): enable service discovery with @EnableDiscoveryClient

--- src/main/java/com/fawry/user_api/UserApiApplication.java
+++ src/main/java/com/fawry/user_api/UserApiApplication.java
@@ -2,8 +2,10 @@ package com.fawry.user_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApiApplication {
 
 	public static void main(String[] args) {
