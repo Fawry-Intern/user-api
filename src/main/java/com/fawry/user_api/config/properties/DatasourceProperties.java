dd63fd923e8079b410082ad9ba3b8fdf7fda92f8
Hossam Hatem <7ossam7atem1@gmail.com>
2025-03-06 12:44:37 +0200

    chore: use lombok annotations instead of plain getters & setters

--- src/main/java/com/fawry/user_api/config/properties/DatasourceProperties.java
+++ src/main/java/com/fawry/user_api/config/properties/DatasourceProperties.java
@@ -1,46 +1,17 @@
 package com.fawry.user_api.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "custom.datasource")
+@Getter
+@Setter
 public class DatasourceProperties {
-
     private String driver;
     private String url;
     private String username;
     private String password;
-
-    public String getDriver() {
-        return driver;
-    }
-
-    public void setDriver(String driver) {
-        this.driver = driver;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
