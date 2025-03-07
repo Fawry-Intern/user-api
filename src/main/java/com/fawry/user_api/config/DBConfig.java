92b86e2ac8bc6b8700d1be4ed744a12d4afd2754
Muhammad Hussein <muhammadhussein2312@gmail.com>
2025-03-08 01:42:44 +0200

    feat(Database): Configure Liquibase for User API

--- src/main/java/com/fawry/user_api/config/DBConfig.java
+++ src/main/java/com/fawry/user_api/config/DBConfig.java
@@ -4,13 +4,10 @@ import com.fawry.user_api.config.properties.DatasourceProperties;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
 
 import javax.sql.DataSource;
-import java.util.Properties;
+
 
 @Configuration
 public class DBConfig {
@@ -25,13 +22,13 @@ public class DBConfig {
     public DataSource dataSource() {
         var dataSource = new HikariDataSource();
 
-       dataSource.setDriverClassName(properties.getDriver());
-       dataSource.setJdbcUrl(properties.getUrl());
-       dataSource.setUsername(properties.getUsername());
-       dataSource.setPassword(properties.getPassword());
-       dataSource.setMaximumPoolSize(properties.getMaxPoolSize());
-       dataSource.setMaxLifetime(properties.getMaxLifeTime());
-       return dataSource;
+        dataSource.setDriverClassName(properties.getDriver());
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        dataSource.setMaximumPoolSize(properties.getMaxPoolSize());
+        dataSource.setMaxLifetime(properties.getMaxLifeTime());
+        return dataSource;
     }
 
 }
--- src/main/java/com/fawry/user_api/config/properties/DatasourceProperties.java
+++ src/main/java/com/fawry/user_api/config/properties/DatasourceProperties.java
@@ -17,5 +17,52 @@ public class DatasourceProperties {
     private int maxPoolSize;
     private int maxLifeTime;
 
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public int getMaxLifeTime() {
+        return maxLifeTime;
+    }
+
+    public void setMaxLifeTime(int maxLifeTime) {
+        this.maxLifeTime = maxLifeTime;
+    }
 }
 
